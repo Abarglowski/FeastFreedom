279adf2dbf423d89152fdb516ad371c590e640d9
xpetrus <phsiao24@gmail.com>
2019-09-24 13:31:11 -0400

    token auth

--- FeastFreedom_Angular/FeastFreedom/src/app/login/login.component.ts
+++ FeastFreedom_Angular/FeastFreedom/src/app/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
+  input;
 
-  constructor() { }
+  constructor(private userService: UserService ) { }
 
   ngOnInit() {
+    this.input = {
+      username: '', // username
+      password: ''
+    };
+  }
+
+  loginUser() {
+    this.userService.loginUser(this.input).subscribe(
+      response => {
+        console.log(response);
+        alert('User ' + this.input.username + ' has been logged in!');
+      },
+      error => console.log('error', error)
+    );
   }
 
 }
