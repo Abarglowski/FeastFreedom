e08928f0200d7ec45c8b864cfe40d71026df5ce7
xpetrus <phsiao24@gmail.com>
2019-09-23 23:24:24 -0400

    custom user model

---
+++ FreedomFeast_Angular/FreedomFeast/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
+})
+export class RegisterComponent implements OnInit {
+  register;
+
+  constructor(private userService: UserService ) { }
+
+  ngOnInit() {
+    this.register = {
+      email: '',
+      full_name: '',
+      password: ''
+    };
+  }
+
+  registerUser() {
+    this.userService.registerUser(this.register).subscribe(
+      response => {
+        alert('User ' + this.register.email + ' has been created!');
+      },
+      error => console.log('error', error)
+    );
+  }
+
+
+}
